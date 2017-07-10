a2174d70531f880ba0a4695d5c1fa5f9c1aff461
{Yuniha} <{kasuanotsuta@gmail.com}>
2017-07-11 03:17:31 +0900

    modalwindow test

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 function main () {
 	addFlickrButton ();
-
 	$("#breadcrumb")[0].innerHTML = $("#breadcrumb")[0].innerHTML.replace(/日記/,"ダイアリー");
 	
 	console.log ();
@@ -11,6 +10,9 @@ function main () {
 
 	requestSearch(testAddress);
 
+	
+	$("body").append('<div id="modal-content" style="opacity: 1; position: absolute; top: 166px; left: 294.5px;"><p>「閉じる」か「背景」をクリックするとモーダルウィンドウを終了します。</p><p><a id="modal-close" class="button-link">閉じる</a></p></div>');
+	
 
 }
 
@@ -27,10 +29,57 @@ function addFlickrButton () {
 }
 
 function onClickButtonFlickr () {
-	alert ("Welcome to Flickr");
+	$(this).blur() ;	//ボタンからフォーカスを外す
+	if($("#modal-overlay")[0]) return false ;		//新しくモーダルウィンドウを起動しない [下とどちらか選択]
+
+	//オーバーレイ用のHTMLコードを、[body]内の最後に生成する
+	$("body").append('<div id="modal-overlay"></div>');
+
+	//[$modal-overlay]をフェードインさせる
+	$("#modal-content").fadeIn("slow");
+	$("#modal-overlay").fadeIn("slow");
+
+	centeringModalSyncer();
+
+	$("#modal-overlay,#modal-close").unbind().click(function(){
+		//[#modal-overlay]と[#modal-close]をフェードアウトする
+		$("#modal-content,#modal-overlay").fadeOut("slow",function(){
+			//フェードアウト後、[#modal-overlay]をHTML(DOM)上から削除
+			$("#modal-overlay").remove();
+		});
+	});
 }
 
 
+
+//センタリングをする関数
+function centeringModalSyncer(){
+
+	//画面(ウィンドウ)の幅を取得し、変数[w]に格納
+	var w = $(window).width();
+
+	//画面(ウィンドウ)の高さを取得し、変数[h]に格納
+	var h = $(window).height();
+
+	//コンテンツ(#modal-content)の幅を取得し、変数[cw]に格納
+	var cw = $("#modal-content").outerWidth({margin:true});
+
+	//コンテンツ(#modal-content)の高さを取得し、変数[ch]に格納
+	var ch = $("#modal-content").outerHeight({margin:true});
+
+	//コンテンツ(#modal-content)を真ん中に配置するのに、左端から何ピクセル離せばいいか？を計算して、変数[pxleft]に格納
+	var pxleft = ((w - cw)/2);
+
+	//コンテンツ(#modal-content)を真ん中に配置するのに、上部から何ピクセル離せばいいか？を計算して、変数[pxtop]に格納
+	var pxtop = ((h - ch)/2);
+
+	//[#modal-content]のCSSに[left]の値(pxleft)を設定
+	$("#modal-content").css({"left": pxleft + "px"});
+
+	//[#modal-content]のCSSに[top]の値(pxtop)を設定
+	$("#modal-content").css({"top": pxtop + "px"});
+
+}
 
 function requestSearch(uri) {
 	console.log(uri);
