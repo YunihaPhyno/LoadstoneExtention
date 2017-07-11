35bc888f9afb279b782f66845b73c9dd2a767b80
{Yuniha} <{kasuanotsuta@gmail.com}>
2017-07-12 03:18:23 +0900

    リファクタリング、ウィンドウの背景、表示位置調節関数

--- main.js
+++ main.js
@@ -1,94 +1,109 @@
 (window.onload = main);
 var btnFlickrDOMId = "ButtonFlickr";
-var modalWindowFlickrDomId = "modal-content";
-
-
+var modalWindowFlickrDomId = "modalWindowFlickr";
+var overlayDomId = "modalOverlayFlickr";
+var imgSelectBoxDomId = "imgSelectBoxFlickr";
+var userId = "152412377@N03";
+var testAddress = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8013aa7e65fc72888f4f1eae7dadc448&user_id=152412377%40N03&format=rest";
 
 function main () {
+	//モーダルウィンドウの追加
+	createModalWindow();
+
+	//フリッカーボタンの追加
 	addFlickrButton ();
-	
-	console.log ();
-	var userId = "152412377@N03";
-	var testAddress = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7633b48cbee42b2fed0aa22b08ea8b3c&user_id=152412377%40N03&format=rest";
-
-	$("body").append('<div id="' + modalWindowFlickrDomId + '" style="opacity: 1; position: absolute; top: 166px; left: 642.5px;"><p>「閉じる」か「背景」をクリックするとモーダルウィンドウを終了します。</p><p><a id="modal-close" class="button-link">閉じる</a></p></div>');
-	
-	createModalContentWindow();
 }
 
-function createModalContentWindow () {
-	$("body").append('<div id="' + modalWindowFlickrDomId + '" style="opacity: 1; position: absolute; top: 166px; left: 642.5px;"></div>');
-	
+//----------------モーダルウィンドウ関係--------------------
+function createModalWindow () {
+	//DOMの追加
+	$("body").append('<div id="' + modalWindowFlickrDomId + '"></div>');
+	//$("body").append('<div id="' + modalWindowFlickrDomId + '" class="sys_img_select_box" style="opacity: 1; position: absolute; top: 166px; left: 642.5px;"><ul class="upload__list clearfix sys__upload__list"></ul><ul class="btn__pager sys_upload_pager"><li><a href="javascript:void(0);" class="icon-list__pager btn__pager__prev--all js__tooltip" data-tooltip="先頭へ"></a></li><li><a href="javascript:void(0);" class="icon-list__pager btn__pager__prev js__tooltip" data-tooltip="前へ"></a></li><li class="btn__pager__current sys_current_page"></li><li><a href="javascript:void(0);" class="icon-list__pager btn__pager__next js__tooltip" data-tooltip="次へ"></a></li><li><a href="javascript:void(0);" class="icon-list__pager btn__pager__next--all js__tooltip" data-tooltip="最後へ"></a></li></ul><a href="javascript:void(0);" class="btn__color--radius parts__space--reset sys_upload_submit">選択画像で決定</a></div>');
 	var mordalWindow = $("#" + modalWindowFlickrDomId);
+	createTitle (mordalWindow);
+	createImageSelectBox (mordalWindow);
+}
+
+function createTitle (mordalWindow) {
 	mordalWindow.append ('<h3 class="heading--lg parts__space--add">画像を選択</h3>');
-
-	var imgSelectBoxDomId = "img_select_box_flickr";
-	mordalWindow.append ('<div id="' + imgSelectBoxDomId + '" class="sys_img_select_box"></div>');
-	var imgSelectBox = mordalWindow.children("#" + imgSelectBoxDomId);
-	imgSelectBox.append('<ul class="upload__list clearfix sys__upload__list"></ul>');
-	requestSearch(testAddress);
 }
 
+function createImageSelectBox (mordalWindow) {
+	mordalWindow.append ('<div id="' + imgSelectBoxDomId + '" class="sys_img_select_box"><ul class="clearfix"></ul></div>');
+}
+
+//----------------フリッカーボタン関係------------------
 function addFlickrButton () {
+	//img_selectは遅延読み込みされるため読み込みを待つ
 	var title = $("#img_select");
-	if (title == undefined) {
-		setTimeout(testloop,1000);
+	if (!title) {
+		setTimeout(addFlickrButton,1000);
 	}
-	$('<ul><li><a href="javascript:void(0);" class="btn__color--radius" id=' + btnFlickrDOMId + '>Flickr画像を参照</a></li></ul>').insertAfter('#container>ul');
-	var btnFlickr = document.getElementById(btnFlickrDOMId);
-	btnFlickr.addEventListener("click", onClickButtonFlickr, false);
+
+	//DOM埋め込み
+	$('<ul><li><a href="javascript:void(0);" class="btn__color--radius" id=' + btnFlickrDOMId + '>Flickr画像を参照</a></li></ul>').insertAfter('#container>ul.upload__btn-2');
+
+	//クリックイベント埋め込み
+	document.getElementById(btnFlickrDOMId).addEventListener("click", onClickFlickrButton, false);
 }
 
-function onClickButtonFlickr () {
-	$(this).blur() ;	//ボタンからフォーカスを外す
-	if($("#modal-overlay")[0]) return false ;		//新しくモーダルウィンドウを起動しない [下とどちらか選択]
+function onClickFlickrButton () {
+	//ボタンからフォーカスを外す
+	$(this).blur() ;
 
-	//オーバーレイ用のHTMLコードを、[body]内の最後に生成する
-	$("body").append('<div id="modal-overlay"></div>');
+	createOverlay ();
 
-	//[$modal-overlay]をフェードインさせる
-	$("#modal-content").fadeIn("slow");
-	$("#modal-overlay").fadeIn("slow");
+	//フェードイン
+	$("#"+modalWindowFlickrDomId).fadeIn("slow");
+	$("#"+overlayDomId).fadeIn("slow");
+
+	//requestSearch(testAddress);
 
 	centeringModalSyncer();
-
-	$("#modal-overlay,#modal-close").unbind().click(function(){
-		//[#modal-overlay]と[#modal-close]をフェードアウトする
-		$("#modal-content,#modal-overlay").fadeOut("slow",function(){
-			//フェードアウト後、[#modal-overlay]をHTML(DOM)上から削除
-			$("#modal-overlay").remove();
-		});
-	});
 }
 
+function createOverlay () {
+	//新しくモーダルウィンドウを起動しない
+	if($("#" + overlayDomId)[0]) {
+		return false ;
+	}
 
+	//オーバーレイ用のHTMLコードを、[body]内の最後に生成する
+	$("body").append('<div id="' + overlayDomId + '"></div>');
+
+	$("#"+overlayDomId).click(onClickOverlay);
+}
+
+function onClickOverlay () {
+	//フェードアウトさせる
+	$("#"+overlayDomId+",#"+modalWindowFlickrDomId).fadeOut("slow",afterFadeOutOverlay);
+}
+
+function afterFadeOutOverlay () {
+	//フェードアウト後、[#modal-overlay]をHTML(DOM)上から削除
+	$("#"+overlayDomId).remove();
+}
 
 //センタリングをする関数
 function centeringModalSyncer(){
+	//ウィンドウサイズ
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
 
-	//画面(ウィンドウ)の幅を取得し、変数[w]に格納
-	var w = $(window).width();
-
-	//画面(ウィンドウ)の高さを取得し、変数[h]に格納
-	var h = $(window).height();
-
-	//コンテンツ(#modal-content)の幅を取得し、変数[cw]に格納
-	var cw = $("#modal-content").outerWidth({margin:true});
-
-	//コンテンツ(#modal-content)の高さを取得し、変数[ch]に格納
-	var ch = $("#modal-content").outerHeight({margin:true});
+	//モーダルウィンドウのサイズ
+	var modalWindow = $("#"+modalWindowFlickrDomId);
+	var modalHeight = $("#"+modalWindowFlickrDomId).outerHeight(true);
+	var modalWidth = $("#"+modalWindowFlickrDomId).outerWidth(true);
 
 	//コンテンツ(#modal-content)を真ん中に配置するのに、左端から何ピクセル離せばいいか？を計算して、変数[pxleft]に格納
-	var pxleft = ((w - cw)/2);
+	var pxleft = ((windowWidth - modalWidth)/2);
+	var pxtop = $(window).scrollTop() - modalHeight * 2 + 30;//((windowHeight - modalHeight)/2);
 
-	//コンテンツ(#modal-content)を真ん中に配置するのに、上部から何ピクセル離せばいいか？を計算して、変数[pxtop]に格納
-	var pxtop = ((h - ch)/2);
+	console.log($(window).scrollTop());
 
 	//[#modal-content]のCSSに[left]の値(pxleft)を設定
-	$("#modal-content").css({"left": pxleft + "px"});
-
-	//[#modal-content]のCSSに[top]の値(pxtop)を設定
-	$("#modal-content").css({"top": pxtop + "px"});
+	modalWindow.css({"left": pxleft + "px"});
+	modalWindow.css({"top": pxtop + "px"});
 
 }
 
@@ -112,15 +127,12 @@ function readyStateChange(event) {
 			}
 		}
 	}
-
 }
 
 function getResults(data) {
 	var photos = data.getElementsByTagName('photo');
 	var str = '';
 	console.log(photos);
-
-	$("#"+modalWindowFlickrDomId).append('<ul class="upload__list clearfix sys__upload__list"></ul>');
 	for (var i = 0, count = photos.length; i < count; i++) {
 
 		var farmId = photos[i].getAttribute('farm');
@@ -133,7 +145,7 @@ function getResults(data) {
 		console.log(url);
 		image.src = url;
 		
-		$("#"+modalWindowFlickrDomId+">ul").append('<li class="js__images" data-uri="' + url + '" data-isexteral style="cursor: pointer;"><img src="' + url + '"></li>');
+		$("#"+imgSelectBoxDomId+">ul").append('<li class="js__images" data-isexteral style="cursor: pointer;"><img src="' + url + '" width="110"></li>');
 		if (i > 10) break;
 	}
 }
