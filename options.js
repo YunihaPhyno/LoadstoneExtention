cdafd9c2575515b581b0b6c988e87b71a5bd4fe8
Yuniha Phyno <yunihaphyno@gmail.com>
2017-08-11 02:29:29 +0900

    定型文機能追加

--- options.js
+++ options.js
@@ -3,25 +3,30 @@ $(function(){
   // セーブボタンが押されたら、
   // ローカルストレージに保存する。
   $("#save").click(function () {
-    chrome.storage.sync.set({"flickrUserId" : $("#flickrUserId").val()}, OnSavedFlickrUserId);
+    chrome.storage.sync.set({"flickrUserId" : $("#flickrUserId").val(), "fixedPhrase": $("#fixedPhrase").val()}, OnClickSavedButton);
   });
 
 
   // オプション画面の初期値を設定する
-  chrome.storage.sync.get(['flickrUserId'], OnLoadFlickrUserId);
+  chrome.storage.sync.get(["flickrUserId", "fixedPhrase"], OnLoad);
 });
 
-function OnSavedFlickrUserId () {
+function OnClickSavedButton () {
   console.log ("save succeed!!");
+  alert ("保存完了！\n\nFlickr\n" + $("#flickrUserId").val() + "\n\n定型文\n" + $("#fixedPhrase").val());
 }
 
-function OnLoadFlickrUserId (items) {
-  console.log(items["flickrUserId"]);
+function OnLoad (items) {
+  console.log(items);
 
   if (items["flickrUserId"]) {
     $("#flickrUserId").val(items["flickrUserId"]);
   } else {
     $("#flickrUserId").val("123456789@N01");
+  }
+
+  if (items["fixedPhrase"]) {
+    $("#fixedPhrase").val(items["fixedPhrase"]);
   }
   console.log ("load succeed!!");
 }
