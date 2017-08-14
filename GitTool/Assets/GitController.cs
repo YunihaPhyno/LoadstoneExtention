aabe8455e5f8a56d7cfc9eff59a9cc3e298b5e5e
Yuniha Phyno <yunihaphyno@gmail.com>
2017-08-15 02:37:19 +0900

    とりあえずaddallまで実装

--- GitTool/Assets/GitController.cs
+++ GitTool/Assets/GitController.cs
@@ -5,58 +5,39 @@ using System.Text.RegularExpressions;
 
 public class GitController : CommandProcessBase
 {
-    string currentBranchName;
-    string[] branchNames;
+    // constructor
+    public GitController(string gitDirectory) : base(gitDirectory) {}
 
-    public GitController(string gitDirectory) : base(gitDirectory){}
-
-    public string GetCurrentBranchName()
-    {
-        log.d("GitController.GetCurrentBranchName()");
-        UpdateBranchNames();
-        return currentBranchName;
-    }
-
-    public void UpdateBranchNames() {
-        log.d("GitController.GetBranchNames()");
-        string stdout = Exec("git branch");
-        branchNames = stdout.Split('\n');
-        for(int i = 0; i < branchNames.Length; i++)
-        {
-            if (Regex.IsMatch(branchNames[i], @"\*"))
-            {
-                branchNames[i] = Regex.Replace(branchNames[i], @"\* (.*)", "$1");
-                currentBranchName = branchNames[i];
-            }
-        }
-        log.d(branchNames);
-    }
-
-    public string[] GetBranchNames()
-    {        
-        UpdateBranchNames();
-        return branchNames;
-    }
-
-    public string Status()
-    {
-        return Exec("git status");
-    }
-
-    public string Pull()
-    {
-        return Exec("git pull");
-    }
-
-    public string Push()
-    {
-        return Exec("git push");
-    }
-
-    public string Add(string[] filenames)
+    public void Status()
     {
         Exec("git status");
-        return "add files(未実装)";
-        //return Exec("git Add");
     }
+
+    public void Pull()
+    {
+        Exec("git pull");
+    }
+
+    public void Push()
+    {
+        Exec("git push");
+    }
+
+    public void Add(string[] filenames)
+    {
+        string command = "git add ";
+        for (int i = 0; i < filenames.Length; i++)
+        {
+            command += " " + filenames[i];
+        }
+        Exec(command);
+    }
+
+    public void Branch()
+    {
+        Exec("git branch");
+    }
+
+
+    
 }
