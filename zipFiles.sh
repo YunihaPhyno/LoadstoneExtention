b38da47d178607b93e8129156d4351e4c4aa7b10
Yuniha Phyno <yunihaphyno@gmail.com>
2017-10-27 02:31:45 +0900

    zipFiles.sh追加

---
+++ zipFiles.sh
@@ -0,0 +1,11 @@
+cd
+zip ./LoadstoneExtention.zip \
+icons/icon128.png \
+icons/icon16.png \
+icons/icon48.png \
+jquery/jquery-3.2.1.min.js \
+main.js \
+manifest.json \
+modal-test.css \
+options.html \
+options.js
