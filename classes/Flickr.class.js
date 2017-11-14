56089492da8311589e3a2d5def3ab817e3f55841
Yuniha Phyno <yunihaphyno@gmail.com>
2017-11-15 03:54:36 +0900

    Chrome class 追加

--- classes/Flickr.class.js
+++ classes/Flickr.class.js
@@ -1,9 +1,21 @@
 Debug.log("load:Flickr.class.js");
 
 class Flickr {
-	constructor (userid) {
+	constructor (userid, nImgsPerPage) {
 		this.userId = userid;
-
+		this.perPage = nImgsPerPage;
 		Debug.log ("flickr id : " + this.userId);
+	}
+
+	GetAPIURL (pageNum = 0) {
+		return GetBaseUrl() + "&" + "api_key=" + GetApiKey() + "&" + "user_id=" + this.userId + "&" + "per_page=" + this.perPage + "&" + "page=" + pageNum + "&format=rest";
+	}
+
+	static GetBaseUrl () {
+		return "https://api.flickr.com/services/rest/?method=flickr.photos.search";
+	}
+
+	static GetApiKey () {
+		return "730f880c45aed14a1e0cee8ff851b4d2";
 	}
 }
