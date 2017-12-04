b4959818207e126ec7daf925424dae14b27c1915
Yuniha Phyno <yunihaphyno@gmail.com>
2017-12-05 02:02:00 +0900

    DocumentObjectBase Class継承

--- classes/ModalWindow.class.js
+++ classes/ModalWindow.class.js
@@ -1,1 +1,19 @@
 Debug.log("load:ModalWindow.class.js");
+
+// like Abstruct Class
+class DocumentObjectBase {
+
+	constructor (id) {
+		this.id_ = id;
+	}
+
+	GetId () {
+		return this.id_;
+	}
+}
+
+class ModalWindow extends DocumentObjectBase {
+	constructor (id, parent) {
+		super (id);
+	}
+}
