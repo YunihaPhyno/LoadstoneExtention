8da6e5afb5663ec05ad41d03d988cca60c47980f
Yuniha Phyno <yunihaphyno@gmail.com>
2018-01-19 17:57:14 +0900

    オーバーレイのクラスが一応完成

--- classes/ModalWindow.class.js
+++ classes/ModalWindow.class.js
@@ -1,4 +1,9 @@
+// あくまでLODESTONE上で動作する
 Debug.log("load:ModalWindow.class.js");
+
+function ID (id) {
+	return "#" + id;
+}
 
 // like Abstruct Class
 class DocumentObjectBase {
@@ -19,11 +24,61 @@ class ModalWindow extends DocumentObjectBase {
 	constructor (id, parent) {
 		super (id);
 		this.parent_ = parent;
-		this.Create_ ();
+		this.CreateObjects_ ();
 	}
 
-	Create_ () {
-		this.root_ = this.parent_.append ('<div id="' + this.id_ + '"></div>');
-		this.title_ = this.root.append ('<div id="' + this.id_ + "" + '"></div>');
+	CreateObjects_ () {
+		// ルートDOMの作成
+		this.CreateRoot_ ();
+
+		// オーバーレイの作成
+		this.overlay = new ModalOverlay (this.root_,100009);
+		this.overlay.Visible();
+
+		// タイトル
+		//this.title_ = this.root.append ('<div id="' + this.id_ + "" + '"></div>');
+	}
+
+	CreateRoot_ () {
+		this.parent_.append ('<div id="' + this.id_ + '"></div>');
+		this.root_ = $(ID(this.id_));
 	}
 }
+
+class ModalOverlay {
+	// parent : jquery object
+	constructor (parent,z_idx,max_opac = 0.6) {
+		this.parent_ = parent;
+		this.max_opacity_ = max_opac;
+		this.Create_ (z_idx);
+
+		// アニメーション登録
+		this.root_.bind("click", {obj:this}, function (event){event.data.obj.Hidden()});
+	}
+
+	Create_ (z_idx) {
+		this.parent_.append ('<div class="modal_overlay" style="z-index: ' + z_idx + '; opacity: ' + this.max_opacity_ + '; display: none;"></div>');
+		this.root_ = this.parent_.children (".modal_overlay");
+		Debug.log (this.root_);
+	}
+
+	SetActive_ (flag) {
+		if (flag) {
+			this.root_.css ("visibility", "visible");
+		} 
+
+		else {
+			
+		}
+	}
+
+	Visible () {
+		this.root_.fadeIn("slow");
+	}
+
+	Hidden () {
+		this.root_.fadeOut("slow");
+	}
+
+}
+
